6759e1e3771f88c4c0a2a9382073b57c66033a9a
Kien.Nguyentrung <Kien.Nguyentrung@HAND3RFQ574>
2026-03-04 16:01:19 +0700

    Add project files.

---
+++ ShareLib/Model/IncidentTicket.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareLib.Model
+{// Định nghĩa 5 trạng thái cốt lõi của preAndon
+    public enum TicketStatus
+    {
+        Green = 0,      // Bình thường
+        Yellow = 1,     // Cảnh báo (vẫn chạy)
+        Red = 2,        // Dừng máy
+        Repairing = 3,  // KTV đang sửa
+        WaitLeader = 4, // Chờ xác nhận
+        Closed = 5      // Đã đóng
+    }
+
+    public class IncidentTicket
+    {
+        // Thông tin cơ bản
+        public string? TicketId { get; set; }
+        public string? LineNumber { get; set; }
+        public string? StationName { get; set; }
+
+        // Trạng thái hiện tại
+        public TicketStatus Status { get; set; } = TicketStatus.Green;
+
+        // Dữ liệu thời gian (Rất quan trọng để tính KPI MTTR/MTBF)
+        public DateTime? ReportedAt { get; set; }
+        public DateTime? TechCheckinAt { get; set; }
+        public DateTime? LeaderConfirmedAt { get; set; }
+
+        // Thông tin con người (Để bạn quản lý hiệu suất)
+        public string? OperatorName { get; set; }
+        public string? TechnicianName { get; set; }
+
+        // Mã lệnh sản xuất đang chạy/ mã sản phẩm 
+        public string? WorkOrder { get; set; }
+        public string? Product { get;set; }
+    }
+}
---
+++ ShareLib/Severs/IncidentService.cs
@@ -0,0 +1,72 @@
+﻿
+using ShareLib.Model;
+using System;
+using System.Data.SQLite;
+
+namespace SharedLib.Services
+{
+    public class IncidentService
+    {
+        private readonly string _connectionString;
+
+        // Nhận đường dẫn file database (.db) khi khởi tạo
+        public IncidentService(string dbFilePath)
+        {
+            _connectionString = $"Data Source={dbFilePath};Version=3;";
+            CreateTableIfNotExists(); // Tự động tạo bảng nếu chưa có
+        }
+
+        // 1. Lệnh tạo bảng SQLite
+        private void CreateTableIfNotExists()
+        {
+            using (var conn = new SQLiteConnection(_connectionString))
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        CREATE TABLE IF NOT EXISTS Tickets (
+                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            TicketId TEXT UNIQUE,
+                            LineNumber TEXT,
+                            AlarmTypeIndex INTEGER,
+                            Status INTEGER,
+                            ReportedAt TEXT,
+                            OperatorName TEXT,
+                            WorkOrder TEXT,
+                            Product TEXT
+
+                        )";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // 2. Lệnh Mở Ticket mới (Lưu xuống DB)
+        public void OpenIncident(IncidentTicket ticket)
+        {
+            using (var conn = new SQLiteConnection(_connectionString))
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    // LUÔN dùng @parameter để truyền biến vào SQL
+                    cmd.CommandText = @"
+                        INSERT INTO Tickets (TicketId, LineNumber, AlarmTypeIndex, Status, ReportedAt, OperatorName, WorkOrder, Product )
+                        VALUES (@TicketId, @LineNumber, @AlarmTypeIndex, @Status, @ReportedAt, @OperatorName, @WorkOrder, @Product)";
+
+                    cmd.Parameters.AddWithValue("@TicketId", ticket.TicketId);
+                    cmd.Parameters.AddWithValue("@LineNumber", ticket.LineNumber);
+                    cmd.Parameters.AddWithValue("@AlarmTypeIndex", ticket.AlarmTypeIndex);
+                    cmd.Parameters.AddWithValue("@Status", (int)ticket.Status);
+                    cmd.Parameters.AddWithValue("@ReportedAt", ticket.ReportedAt.ToString("yyyy-MM-dd HH:mm:ss"));
+                    cmd.Parameters.AddWithValue("@OperatorName", ticket.OperatorName);
+                    cmd.Parameters.AddWithValue("@Product", ticket.Product);
+                    cmd.Parameters.AddWithValue("@WorkOrder", ticket.WorkOrder);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
