04a5f73f58a9b2addf1db061c7e6c66070ee9858
boyd.sonnic <boyd.sonnic@gmail.com>
2026-03-26 09:20:48 +0700

    git push --set upsteam origin main

---
+++ AndonDashBroad/Views/Kpi/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AndonDashBroad.Views.Kpi
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
