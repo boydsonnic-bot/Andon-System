3446c5ad9e25b4cb1304f37e917794892d865591
boyd.sonnic <boyd.sonnic@gmail.com>
2026-03-30 11:20:43 +0700

    git push --set stream origin main

---
+++ AndonDashBroad/Controllers/SpcController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AndonDashBroad.Controllers
+{
+    public class SpcController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Trả về file giao diện Views/Spc/Index.cshtml
+            return View();
+        }
+    }
+}
